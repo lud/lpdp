52ae3ff7f6831aa75723cbf661dae0ac2ff6dc42
lud <ludovic@demblans.com>
2015-02-14 02:52:56 +0100

    bugfix highlightjs lud-theme

--- resources/lud-theme/_themefile.php
+++ resources/lud-theme/_themefile.php
@@ -3,7 +3,7 @@
 return [
 	'styles' => [
 		asset('packages/lud/press/lib/css/yeti-bootstrap.min.css'),
-		'//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/styles/atelier-dune.light.css',
+		'//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/styles/tomorrow.min.css',
 		asset('lud/css/lud.css'),
 	],
 	'scripts' => [
