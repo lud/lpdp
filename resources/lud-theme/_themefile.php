436b3066ca43e78a60c0b27e7aa384b10c034f15
niahoo <dev@ooha.in>
2015-02-19 13:26:56 +0100

    use the new theme asset publish features

--- resources/lud-theme/_themefile.php
+++ resources/lud-theme/_themefile.php
@@ -4,11 +4,14 @@ return [
 	'styles' => [
 		asset('packages/lud/press/lib/css/yeti-bootstrap.min.css'),
 		'//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/styles/tomorrow.min.css',
-		asset('lud/css/lud.css'),
+		asset('packages/lud-theme/css/lud.css'),
 	],
 	'scripts' => [
 		'//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js',
 		'//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/highlight.min.js',
 		asset('packages/lud/press/lib/js/bootstrap.min.js'),
+	],
+	'publishes' => [
+		__DIR__.'/public' => base_path('public/packages/lud-theme'),
 	],
 ];
