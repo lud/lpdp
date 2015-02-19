a8f0276d8db5f0ddb5b9c1319f4527d56a628157
lud <ludovic@demblans.com>
2015-02-19 01:42:17 +0100

    use press route helpers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,3 +6,6 @@ Route::controllers([
 	'auth' => 'App\Http\Controllers\Auth\AuthController',
 	'password' => 'App\Http\Controllers\Auth\PasswordController',
 ]);
+
+\Press::listRoute('tag/{tag}', 'tag|sort', ['as' => 'press.tag']);
+\Press::listRoute('/', 'all|sort:date,desc', ['as' => 'press.home']);
